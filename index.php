3f89990831ed76f3f1c04dbfd25b36bfc7fc07bd
iMacwww <FelipeGU@iMac-de-Felipe-2.local>
2018-10-02 17:11:28 -0500

    carga2

--- index.php
+++ index.php
@@ -4,37 +4,12 @@
   <meta charset="utf-8">
   <meta name="description">
   <meta name="keywords">
-  <link rel="icon" type="image/png" href="images/logo.png">
-  <title>GESTIÓN DE NEGOCIOS ROMANA S.A. DE C.V.</title>
+  <title>Rogelio García Martínez</title>
   <link href="css/style.css" rel="stylesheet"/>
   <link href="https://fonts.googleapis.com/css?family=Krub" rel="stylesheet">
   <script src="js/jquery-3.3.1.min.js"/></script>
-  <script src="js/load_pages.js"/></script>
 </head>
-<body id="top">
-
-
-      <!--usamos h3 para crear cada menu-->
-      <nav class="">
-          <ul class="menu">
-            <li><a href="index.php"><img src="images/logo.png" class="logo"></a></li>
-            <li id="inicio">Inicio</li>
-            <li id="quienes">Romana</li>
-            <li id="servicios">Servicios</li>
-            <li id="contacto">Contacto</li>
-          </ul>
-      </nav>
-
-      <div id="maincontent">
-
-      </div>
-
-
-      <footer>
-        <div class="container">
-          <p>(c) 2018 - Gestión de Negocios Romana S.A. DE C.V.</p>
-        </div>
-      </footer>
-
+<body>
+  <iframe src="files/p1.pdf" class="ifram"></iframe>
 </body>
 </html>
